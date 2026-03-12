33d1112b9b22b5ffec2324314e5c9dd96ae64713
Dila Efendioğlu <dilaefendioglu@gmail.com>
2026-03-13 00:01:48 +0300

    Add input field with button to display typed text

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Button from "./components/Button";
 function App() {
   // const [değişken, degiskeni setlemek istediğin fonksiyon] = useState()
 
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Sonuç için Tıkla")
-  const title = "BAHİS (Bu Sene Kim Şampiyon Olur )"
+  const [inputName, setInputName] = useState(null)
+  const [data, setData] = useState(null)
+  console.log(inputName, "inputName")
+
+
+  const clickFunc = () => {
+    setData(inputName)
+  }
+
 
   return (
     <>
-      <div>{title}</div>
-      <Button name={"AZALT"} onClick={() => setCount(count - 1)} />
-      <Button name={"ARTTIR"} onClick={() => setCount(count + 1)} />
-      <div>{count}</div>
-      <div onClick={() => setName("FENERBAHÇE")}>{name}</div>
+      <input type="text" onChange={(e) => setInputName(e.target.value)} />
+      <button onClick={clickFunc}>TIKLA</button>
+      {data}
     </>
   )
 }
 export default App
+
